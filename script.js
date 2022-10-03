02366adb82e6a0b6e69e56c31afeaa148cf8c260
Juankleber <112591295+Juankleber@users.noreply.github.com>
2022-10-03 19:57:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function respdata() {
+    var semana = document.getElementById("nsemana")
+    var res = document.getElementById("resp1")
+
+    if(semana.value != 96 ){
+        window.alert("errou, tente novamente")
+        res.innerHTML = 'resultado...'
+    } else {
+        res.innerHTML = `BOAAAAA MEU AMOR, desde o nosso primeiro "oi" até agora já foram 94 semanas, 94 semanas de um amor que veio crescendo, 94 semanas de compreensão, 94 semanas de uma vida compartilhada, de conhecimentos passados, de um sonho realizado, desde que senti o seu primeiro abraço, seu primeiro beijo e contato, foi algo difícil de explicar de tão bom, senti meus sentimentos florescer, uma paz gigantesca e me senti amado de uma forma que sei lá véi, nunca imaginei sentir algo tão bom quando senti naquele momento e sinto até hoje, vivo sempre esperando pelo dia de te ver, vida, te ter por perto é tão bom, eu me sinto completo.<br>
+        Eu amo nosso amor, as músicas q dedicamos um ao outro, quando escutamos músicas no discord, assistir juntos, bagaçar jogando, tudo véi.<br>
+        Amor, que seja mais uma semana de muitas pela frente, meu amor por você vem crescendo, eu só ter um futuro de paz com vc, estou aqui te ver crescendo e poder crescer juntos, construir nossas vidas um ao lado do outro.<br>
+        Sempre que precisar, vou estar aqui, sou seu ombro amigo, sua companhia para todos os momentos, ser e continuar sendo a pessoa que você pode contar sempre.<br>
+        <h2>Eu te amo, minha mulher !</h2>`
+        
+    }
+}
